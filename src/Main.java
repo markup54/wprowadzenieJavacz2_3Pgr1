826c5d8ac4b969e862b53de30ec784c4665f100c
markup54 <marzenakupka@tlen.pl>
2022-09-15 13:12:11 +0200

    zbiór nie zawsze 6 elementów

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-//losowanie 6 liczb
-        //wpisywanie 6 liczb
-        //wypiszemy liczby wpisane i wylosowane
-        System.out.println("Losowanie 6 liczb");
-
-        //tablica
-        int [] wylosowane =new  int[6];
-        //tablice w Java nie mogą mieć zmienianego rozmiaru w trakcie działania
-        //tablice w Java przechowują typy proste lub złożone
-        for(int i=0;i<wylosowane.length;i++){
-            wylosowane[i] =(int)( Math.random()*100 +1);
+        //losowanie bez powtórzeń
+        //kolekcja
+        //w trakcie działania programu można zmieniać rozmiar kolekcji
+        //kolekcja może przechowywać tylko typy złożone
+        //Set - zbiór elementy nie bedą się powtarzać
+        //w zbiorze elementy nie są indeksowane
+        Set<Integer> wylosowane = new HashSet<>();
+        for(int i = 0 ; i<6;i++){
+            wylosowane.add((int)(Math.random()*10));
         }
-        for(int wylosowanaWartosc :wylosowane){
-            System.out.print(wylosowanaWartosc+" ");
-        }
-
-
+        System.out.println("wylosowane:"+wylosowane);
     }
 }
