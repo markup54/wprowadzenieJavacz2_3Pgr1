92a732ac4da907cc84c203f64ef6489c49303398
markup54 <marzenakupka@tlen.pl>
2022-09-15 12:40:49 +0200

    losowanie do tablicy

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+//losowanie 6 liczb
+        //wpisywanie 6 liczb
+        //wypiszemy liczby wpisane i wylosowane
+        System.out.println("Losowanie 6 liczb");
 
-        System.out.println("Hello world!");
+        //tablica
+        int [] wylosowane =new  int[6];
+        //tablice w Java nie mogą mieć zmienianego rozmiaru w trakcie działania
+        //tablice w Java przechowują typy proste lub złożone
+        for(int i=0;i<wylosowane.length;i++){
+            wylosowane[i] =(int)( Math.random()*100 +1);
+        }
+        for(int wylosowanaWartosc :wylosowane){
+            System.out.print(wylosowanaWartosc+" ");
+        }
+
+
     }
 }
